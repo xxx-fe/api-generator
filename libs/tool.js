6f14409c074b97dddf76b3e730f0f1b392c5e44d
subying <tearlight2008@gmail.com>
2016-12-20 16:47:28 +0800

    增加tool工具

---
+++ libs/tool.js
@@ -0,0 +1,57 @@
+/**
+ *@fileOverview tool 工具类
+*/
+const request       = require('request');
+//const BufferHelper  = require('bufferhelper');
+const _             = require('lodash');
+const requestJar    = request.defaults({jar: true});
+
+
+/**
+ * @description get请求
+ * @param {string} url 请求的链接
+ * @param {Object} headers 请求头部
+ * @param {Function} callback 成功回调
+ * @param {Function} errback 失败回调
+ * @returns {Voild}
+*/
+exports.getUrl = function(url,headers,callback,errback){
+    const option = {
+        method: 'GET',
+        url: url,
+        headers:{
+            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36'
+        },
+        encoding: null
+    };
+
+    if(headers && _.isObject(headers)){
+        option.headers = headers;
+    }
+
+    requestJar(option,(error,reponse,body) => {
+        if(error){
+            errback(error);
+        }else{
+            callback(body);
+        }
+    });
+};
+
+
+/**
+ * @description get请求
+ * @param {string} url 请求的链接
+ * @param {Object} headers 请求头部
+ * @returns {Voild}
+*/
+exports.getHttpContent = function(url,headers){
+    return new Promise(function(resolve, reject) {
+        exports.getUrl(url, headers,function(content) {
+            resolve(content);
+        }, function(err) {
+            console.log('getHttpErr='+err,url);
+            reject(err);
+        });
+    });
+};
