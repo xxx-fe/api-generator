3d8b5f4ca10657aa0f36bfcb511733b302fac2ce
李继梁 <ljl@xxxx.com>
2017-02-14 18:33:24 +0800

    修改启动命令

--- bin/run.sh
+++ bin/run.sh
@@ -4,5 +4,5 @@ export PATH=/usr/local/bin:/bin:/usr/bin
 export NODE_PATH=/usr/local/lib/node_modules
 
 pm2 flush
-pm2 start app.js -i max -n --name api-gen --node-args="--harmoney"
+pm2 start app.js -i max -n --name api-gen --node-args="--harmony"
 pm2 logs api-gen
