cb00ba7a53f93c91f643f33193bb73dd7956e2c8
李继梁 <ljl@xxxx.com>
2017-02-28 11:36:05 +0800

    首页说明

--- bin/run.sh
+++ bin/run.sh
@@ -4,5 +4,6 @@
 #export NODE_PATH=/usr/local/lib/node_modules
 
 pm2 flush
-pm2 start app.js -i max -n api-gen --node-args="--harmony"
+pm2 start app.js -i max -n api-gen
+#pm2 start app.js -i max -n api-gen --node-args="--harmony"
 pm2 logs api-gen
